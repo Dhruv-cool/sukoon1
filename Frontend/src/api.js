3186ac5ad55c4031cd3772227c1ca716af5a86fd
Dhruv Khandelwal <135336067+Dhruv-cool@users.noreply.github.com>
2025-07-27 17:22:21 +0530

    Update api.js

--- Frontend/src/api.js
+++ Frontend/src/api.js
@@ -2,7 +2,7 @@
 import axios from "axios";
 
 const API = axios.create({
-  baseURL: "http://localhost:5004/api", // your backend URL
+  baseURL: process.env.REACT_APP_API_BASE_URL, // your backend URL
 });
 
 API.interceptors.request.use((req) => {
