5bdb44271c0f1b4e96531cf46337a71750a4ebaf
Dhruv Khandelwal <135336067+Dhruv-cool@users.noreply.github.com>
2025-07-31 13:42:15 +0530

    Update firebase.jsx

--- Frontend/src/components/firebase.jsx
+++ Frontend/src/components/firebase.jsx
@@ -7,13 +7,12 @@ import { getAnalytics } from "firebase/analytics";
 // Your web app's Firebase configuration
 // For Firebase JS SDK v7.20.0 and later, measurementId is optional
 const firebaseConfig = {
-  apiKey: "AIzaSyAAttonvSoCdu0UAIruIV9domLadtbq1oo",
-  authDomain: "mental-health-app-acce4.firebaseapp.com",
-  projectId: "mental-health-app-acce4",
-  storageBucket: "mental-health-app-acce4.firebasestorage.app",
-  messagingSenderId: "848490109619",
-  appId: "1:848490109619:web:06fb72b40c73c131136ec6",
-  measurementId: "G-H502E15YR6"
+  apiKey: "AIzaSyAVS3904m_nPD0SYKNiIw7fjV1CcoEBI5o",
+  authDomain: "sukoon-app-98987.firebaseapp.com",
+  projectId: "sukoon-app-98987",
+  storageBucket: "sukoon-app-98987.firebasestorage.app",
+  messagingSenderId: "858381221211",
+  appId: "1:858381221211:web:bd4bcacfb6682c33d36e7e"
 };
 
 // Initialize Firebase
