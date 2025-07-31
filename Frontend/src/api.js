b6c16e0be5b6aa72078b6b776a0c229f3b8c2427
Dhruv Khandelwal <135336067+Dhruv-cool@users.noreply.github.com>
2025-07-31 18:39:54 +0530

    Update api.js

--- Frontend/src/api.js
+++ Frontend/src/api.js
@@ -2,7 +2,7 @@
 import axios from "axios";
 
 const API = axios.create({
-  baseURL: process.env.REACT_APP_API_BASE_URL, // your backend URL
+  baseURL: "https://sukoon-backend-927o.onrender.com/api", // your backend URL
 });
 
 API.interceptors.request.use((req) => {
