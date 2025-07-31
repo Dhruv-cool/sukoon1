d30f6bbfbbaab8c6a60f0efbe555d14bd93e1d56
Dhruv Khandelwal <135336067+Dhruv-cool@users.noreply.github.com>
2025-07-31 13:53:58 +0530

    Update and rename firebase-config to firebase-config.js

--- Frontend/src/components/firebase-config.js
+++ Frontend/src/components/firebase-config.js
@@ -10,13 +10,12 @@ import { getAuth, GoogleAuthProvider } from "firebase/auth";
 // Your web app's Firebase configuration
 // For Firebase JS SDK v7.20.0 and later, measurementId is optional
 const firebaseConfig = {
-  apiKey: "AIzaSyB3CyG6V7RcsXBMyYlnzzkLaaU6Ns50vM8",
-  authDomain: "blog-f0cf6.firebaseapp.com",
-  projectId: "blog-f0cf6",
-  storageBucket: "blog-f0cf6.appspot.com",
-  messagingSenderId: "1081352822934",
-  appId: "1:1081352822934:web:0782298fb6e0e9eb6022b7",
-  measurementId: "G-BZKSMLS74X"
+  apiKey: "AIzaSyAVS3904m_nPD0SYKNiIw7fjV1CcoEBI5o",
+  authDomain: "sukoon-app-98987.firebaseapp.com",
+  projectId: "sukoon-app-98987",
+  storageBucket: "sukoon-app-98987.firebasestorage.app",
+  messagingSenderId: "858381221211",
+  appId: "1:858381221211:web:bd4bcacfb6682c33d36e7e"
 };
 
 // Initialize Firebase
